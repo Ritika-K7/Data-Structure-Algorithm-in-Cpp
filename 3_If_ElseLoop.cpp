11ea2137fbcc932b054852a952bce0c81caaf022
Ritika <rkjane333@gmail.com>
2025-02-23 07:14:42 +0530

    if else loop

---
+++ 3_If_ElseLoop.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //EX 1:PACKAGE
+    // int package;
+    // cin>>package;
+    // if(package>10)
+    // {
+    //     cout<<"Accepted";
+    // }
+    // else
+    // {
+    //     cout<<"Rejected";
+    // }
+
+
+
+    // EX 2:MARKS GRADING SYSTEM
+    // int marks;
+    // cin>>marks;
+    // if(marks>33)
+    // {
+    //     cout<<"pass";
+    // }
+    // else
+    // {
+    //     cout<<"Fail";
+    // }
+
+    
+
+    // Compare Two Number
+    // int a,b;
+    // cout<<"Enter first num:";
+    // cin>>a;
+    // cout<<"Enter second num:";
+    // cin>>b;
+    // if (a>b)
+    // {
+    //     cout<<"yes";
+    // }
+    // else
+    // {
+    //     cout<<"No";
+    // }
+    
+    
+
+    int number;
+    cout<<"Enter number:";
+    cin>>number;
+    if(number%2==0)
+    {
+        cout<<"even";
+    }
+    else
+    {
+        cout<<"odd";
+    }
+
+
+}
