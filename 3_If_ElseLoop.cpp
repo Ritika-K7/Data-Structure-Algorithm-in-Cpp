fdb483ce25650470837fd702abc5fdb2b8db854f
Ritika <rkjane333@gmail.com>
2025-02-25 00:00:54 +0530

    modified 3_If_ElseLoop

--- 3_If_ElseLoop.cpp
+++ 3_If_ElseLoop.cpp
@@ -28,7 +28,6 @@ int main()
     // {
     //     cout<<"Fail";
     // }
-
     
 
     // Compare Two Number
@@ -47,18 +46,117 @@ int main()
     // }
     
     
+    // EX 3: Even or Odd
+    // int number;
+    // cout<<"Enter number:";
+    // cin>>number;
+    // if(number%2==0)
+    // {
+    //     cout<<"even";
+    // }
+    // else
+    // {
+    //     cout<<"odd";
+    // }
 
-    int number;
-    cout<<"Enter number:";
-    cin>>number;
-    if(number%2==0)
+
+    /*-----------------------If-elseif-else-----------------------*/
+    
+    // //number is positive negative or 0
+    // int number;
+    // cin>>number;
+    // if(number>0)
+    // {
+    //     cout<<"Positive";
+    // }
+    // else if(number==0)
+    // {
+    //     cout<<"0";
+    // }
+    // else
+    // {
+    //     cout<<"Negative";
+    // }
+
+
+    // //Character is vowel or not(a,e,i,o,u)
+    // char c;
+    // cin>>c;
+    // if(c=='a')
+    // cout<<"vowel";
+    // else if(c=='e')
+    // cout<<"vowel";
+    // else if(c=='i')
+    // cout<<"vowel";
+    // else if(c=='o')
+    // cout<<"vowel";
+    // else if(c=='u')
+    // cout<<"vowel";
+    // else 
+    // cout<<"consonant";
+
+
+    // //WeekDay with number
+    // int n;
+    // cout<<"Enter number:";
+    // cin>>n;
+    // if(n==1)
+    // cout<<"mon";
+    // else if(n==2)
+    // cout<<"Tue";
+    // else if(n==3)
+    // cout<<"wed";
+    // else if(n==4)
+    // cout<<"thu";
+    // else if(n==5)
+    // cout<<"fri";
+    // else if(n==6)
+    // cout<<"Sat";
+    // else 
+    // cout<<"Sun";
+
+
+    // //Print coder army 5 times
+    // for(int count=1;count<=5;count=count+1)
+    // {
+    //     cout<<"Coder army\n";
+    // }
+
+
+    //Print hello 10 times
+    // for(int i=10;i<=29;i=i+1)
+    // {
+    //     cout<<"Hello\n";
+    // }
+     
+
+    //Print 'n' natural number.
+    // int n;
+    // cout<<"Enter num:";
+    // cin>>n;
+    // for(int count=1;count<=n;count=count+1)
+    // {
+    //     cout<<count<<endl;
+    // }
+
+
+    //Print square
+    // int n;
+    // cout<<"Enter num:";
+    // cin>>n;
+    // for(int count=1;count<=n;count=count+1)
+    // {
+    //     cout<<count<<" Square is:"<<count*count<<endl;
+    // }
+
+    
+
+    //Print all even number till 'n'.
+    int n;
+    cout<<"Enter num:";
+    cin>>n;
+    for(int i=2;i<=n;i=i+2)
     {
-        cout<<"even";
+        cout<<i<<endl;
     }
-    else
-    {
-        cout<<"odd";
-    }
-
-
 }
