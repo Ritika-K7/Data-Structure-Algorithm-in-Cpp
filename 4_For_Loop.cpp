6b7e53bad62210a99e0e4cd184051686f595547e
Ritika <rkjane333@gmail.com>
2025-02-27 07:35:18 +0530

    advance loop concept

---
+++ 4_For_Loop.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Print 101 102 ....200
+    // int i;
+    // for (i=101;i<=200;i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+
+    // //Print a to z character
+    // char name;
+    // for(name='a';name<='z';name=name+1)
+    // {
+    //    cout<<name<<" "; 
+    // }
+
+
+    // //Print in reverse order 10,9,8,...1
+    // int n,i;
+    // cout<<"Enter the number:";
+    // cin>>n;
+    // for (i=n;i>=1;i=i-1)
+    // {
+    //     cout<<i<<" ";
+    // }
+
+
+    //AP series 1,4,7,10...100
+    // int i;
+    // for (i=1;i<=100;i=i+3)
+    // {
+    //     cout<<i<<" ";
+    // }
+
+
+    //Table 6,12,18...60
+    // int n,i;
+    // cout<<"Enter the table :";
+    // cin>>n;
+
+    // for(i=1;i<=10;i=i+1)
+    // {
+    //     cout<<n<<"*"<<i<<"="<<n*i<<endl;
+    // }
+
+
+
+    //Power of a number
+    // int n,pow,i,num;
+    // cout<<"enter the number : ";
+    // cin>>n;
+    // cout<<"enter the power : ";
+    // cin>>pow;
+    // num=n;
+    // for(i=1;i<pow;i=i+1)
+    // {
+    //     num=num*n;
+    // }
+    // cout<<num;
+
+
+    //Sum of n natural number
+    // int n ,i,sum=0;
+    // cout<<"Enter the number :";
+    // cin>>n;
+
+    // // for(i=1;i<=n;i=i+1)
+    // // {
+    // //     sum=sum+i;
+    // // }
+    // // cout<<sum;
+    // // OR
+    // cout<<(n*(n+1))/2;
+
+ 
+}
