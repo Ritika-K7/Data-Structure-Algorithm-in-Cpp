6ee6767f7a83d5092ae293c517ea0399be61720c
Ritika <rkjane333@gmail.com>
2025-03-05 07:26:16 +0530

    modified patttern parinting

--- 5_PatternPrinting.cpp
+++ 5_PatternPrinting.cpp
@@ -21,19 +21,92 @@ int main()
 
 
 
-    // 10 10 10 10 10
-    // 10 10 10 10 10
-    // 10 10 10 10 10
-    // 10 10 10 10 10
+    // // 10 10 10 10 10
+    // // 10 10 10 10 10
+    // // 10 10 10 10 10
+    // // 10 10 10 10 10
+    // int row,col;
+    // for(row=1;row<=4;row=row+1)
+    // {
+    //     for(col=1;col<=5;col=col+1)
+    //     {
+    //         cout<<10<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    // // 1111
+    // // 2222
+    // // 3333
+    // // 4444
+    // // 5555
+    // int row,col;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     //Print row 5 times 
+    //     for(col=1;col<=5;col=col+1)
+    //     {
+    //         cout<<row<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+
+
+
+    // // 12345
+    // // 12345
+    // // 12345
+    // // 12345
+    // int row,col;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=1;col<=5;col=col+1)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+    // // 54321
+    // // 54321
+    // // 54321
+    // int row,col;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=5;col>=1;col=col-1)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+
+    // 1 4 9 16 25
+    // 1 4 9 16 25
+    // 1 4 9 16 25
+    // 1 4 9 16 25
     int row,col;
-    for(row=1;row<=4;row=row+1)
+    for(row=1;row<=5;row=row+1)
     {
         for(col=1;col<=5;col=col+1)
         {
-            cout<<10<<" ";
+            cout<<col*col<<" ";
         }
         cout<<endl;
     }
+
+
+    
+
 
 
 
