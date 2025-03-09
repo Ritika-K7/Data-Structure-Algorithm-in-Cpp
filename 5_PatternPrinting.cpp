dfa4023169a40148ee223a44b40e440fbe375142
Ritika <rkjane333@gmail.com>
2025-03-09 16:31:31 +0530

    completed easy pattern printing

--- 5_PatternPrinting.cpp
+++ 5_PatternPrinting.cpp
@@ -151,7 +151,33 @@ int main()
     //     cout<<endl;
     // }
 
+
+    // 1 2 3 4 5
+    // 6 7 8 9 10
+    // 11 12 13 14 15
+    // 16 17 18 19 20
+    // int row,col;
+    // int count=1;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=1;col<=5;col=col+1)
+    //     {
+    //         cout<<count<<" ";
+    //         count=count+1;
+    //     }
+    //     cout<<endl;
+    // }
  
+    //Or
+    // int row,col;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=1;col<=5;col=col+1)
+    //     {
+    //         cout<<(row-1)*5+col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
 
     
 
