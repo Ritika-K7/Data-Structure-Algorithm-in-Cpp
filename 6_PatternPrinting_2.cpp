3be1f418d0129ee34424d186ec4950124476681b
Ritika <rkjane333@gmail.com>
2025-03-28 13:15:28 +0530

    pattern print -2

---
+++ 6_PatternPrinting_2.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    // *
+    // **
+    // ***
+    // int row ,col;
+    
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=1;col<=row;col=col+1)
+    //     {
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    // // 1
+    // // 12
+    // // 123
+    // int row,col;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=1;col<=row;col=col+1)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+
+    // // 1
+    // // 22
+    // // 333
+    // int row,col;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=1;col<=row;col=col+1)
+    //     {
+    //         cout<<row<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+
+    // // 1
+    // // 21
+    // // 321
+    // int row,col;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=row;col>=1;col=col-1)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+
+    // // a
+    // // bb 
+    // // ccc 
+    // // dddd 
+    // int row,col;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     char name='a'+(row-1);
+    //     for(col=1;col<=row;col=col+1)
+    //     {
+    //         cout<<name<<" ";
+    //     }
+    //     cout<<endl;
+
+    // }
+
+
+    // // *****
+    // // ****
+    // // ***
+    // // **
+    // // *
+    // int row,col;
+    // for(row=1;row<=5;row=row+1)
+    // {
+    //     for(col=1;col<=5-(row-1);col=col+1)
+    //     {
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    //---------OR------------
+    // int row,col;
+    // for(row=5;row>=1;row=row-1)
+    // {
+    //     for(col=1;col<=row;col=col+1)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+ }
